1aa3448fb45268eba4a41dae5028a4e2aa2cef02
Leonard Theodorus <aoesotfs@gmail.com>
2023-06-22 14:28:00 +0700

    Initial Commit

---
+++ MC2-United-Hands/MC2_United_HandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2_United_HandsApp.swift
+//  MC2-United-Hands
+//
+//  Created by Leonard Theodorus on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MC2_United_HandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MC2-United-HandsUITests/MC2_United_HandsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MC2_United_HandsUITestsLaunchTests.swift
+//  MC2-United-HandsUITests
+//
+//  Created by Leonard Theodorus on 22/06/23.
+//
+
+import XCTest
+
+final class MC2_United_HandsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
