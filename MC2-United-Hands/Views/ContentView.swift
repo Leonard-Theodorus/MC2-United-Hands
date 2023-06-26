eaac6e538f7dfd9d92d77c318cbc7b528bbe5e1f
Leonard Theodorus <aoesotfs@gmail.com>
2023-06-26 11:15:35 +0700

    Added UserDefaults ViewModel for OnBoarding

--- MC2-United-Hands/Views/ContentView.swift
+++ MC2-United-Hands/Views/ContentView.swift
@@ -8,15 +8,24 @@
 import SwiftUI
 struct ContentView: View {
     @State var pageNavigator : PageNavigation = .expenses
+    @EnvironmentObject var onBoardingVm : OnboardingViewModel
     var body: some View {
-//        NavigationStack{
-//            GeometryReader { geo in
-//                VStack {
-//                    HomeView(width: geo.size.width, height: geo.size.height, pageNavigator: $pageNavigator)
-//                }
-//            }
-//        }
-        ExpensesHorizontalScrollView()
+        //        NavigationStack{
+        //            GeometryReader { geo in
+        //                VStack {
+        //                    HomeView(width: geo.size.width, height: geo.size.height, pageNavigator: $pageNavigator)
+        //                }
+        //            }
+        //        }
+        VStack{
+            ExpensesHorizontalScrollView()
+            if !onBoardingVm.firstTime{
+                Text("FirstTIme")
+            }
+            
+        }
+        
+        
     }
 }
 
