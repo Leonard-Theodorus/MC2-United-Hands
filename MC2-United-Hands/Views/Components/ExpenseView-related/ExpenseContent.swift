2dd671fe30dea7de4ca9b32327842a5f513e0499
Leonard Theodorus <aoesotfs@gmail.com>
2023-06-26 21:18:06 +0700

    Fixed some build file error

---
+++ MC2-United-Hands/Views/Components/ExpenseView-related/ExpenseContent.swift
@@ -0,0 +1,20 @@
+//
+//  ExpenseContent.swift
+//  MC2-United-Hands
+//
+//  Created by Leonard Theodorus on 26/06/23.
+//
+
+import SwiftUI
+
+struct ExpenseContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExpenseContent_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpenseContent()
+    }
+}
