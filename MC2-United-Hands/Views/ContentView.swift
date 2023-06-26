fd2b3c55290b3b81376d3056f86207fbf8785843
priskilladrn <priskilla.adriani@gmail.com>
2023-06-26 18:10:12 +0700

    fix merge

--- MC2-United-Hands/Views/ContentView.swift
+++ MC2-United-Hands/Views/ContentView.swift
@@ -33,5 +33,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .environmentObject(SheetManager())
+            .environmentObject(OnboardingViewModel())
     }
 }
