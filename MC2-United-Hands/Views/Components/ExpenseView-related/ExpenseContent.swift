74cf576dccfb6124c23555209154fb04371351be
Leonard Theodorus <aoesotfs@gmail.com>
2023-06-28 08:59:43 +0700

    Satuin expenseDetail + working view CoreData

--- MC2-United-Hands/Views/Components/ExpenseView-related/ExpenseContent.swift
+++ MC2-United-Hands/Views/Components/ExpenseView-related/ExpenseContent.swift
@@ -15,32 +15,32 @@ struct ExpenseContent: View {
         ScrollView{
             //Today
             if expenses.filter({dayOfDate(for: $0.timestamp ?? Date()) == dayOfDate(for: Date.now)}).count != 0{
-                DailyExpenseView(expenses: expenses, for: Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +))
+                DailyExpenseView(expenses: expenses, date: Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +), isDetailExpenses: $isDetailExpenses)
             }
             //H-1
             if expenses.filter({dayOfDate(for: $0.timestamp ?? Date())  ==
                 dayOfDate(for: Calendar.current.date(byAdding: .day, value: -1, to: Date.now) ?? Date()) }).count != 0{
-                DailyExpenseView(expenses: expenses, for: Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +))
+                DailyExpenseView(expenses: expenses, date: Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +), isDetailExpenses: $isDetailExpenses)
             }
             //H-2
             if expenses.filter({dayOfDate(for: $0.timestamp ?? Date()) == dayOfDate(for: Calendar.current.date(byAdding: .day, value: -2, to: Date.now) ?? Date()) }).count != 0{
-                DailyExpenseView(expenses: expenses, for: Calendar.current.date(byAdding: .day, value: -2, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +))
+                DailyExpenseView(expenses: expenses, date: Calendar.current.date(byAdding: .day, value: -2, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +), isDetailExpenses: $isDetailExpenses)
             }
             //H-3
             if expenses.filter({dayOfDate(for: $0.timestamp ?? Date()) == dayOfDate(for: Calendar.current.date(byAdding: .day, value: -3, to: Date.now) ?? Date())}).count != 0{
-                DailyExpenseView(expenses: expenses, for: Calendar.current.date(byAdding: .day, value: -3, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +))
+                DailyExpenseView(expenses: expenses, date: Calendar.current.date(byAdding: .day, value: -3, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +), isDetailExpenses: $isDetailExpenses)
             }
             //H-4
             if expenses.filter({dayOfDate(for: $0.timestamp ?? Date()) == dayOfDate(for: Calendar.current.date(byAdding: .day, value: -4, to: Date.now) ?? Date())}).count != 0{
-                DailyExpenseView(expenses: expenses, for: Calendar.current.date(byAdding: .day, value: -4, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +))
+                DailyExpenseView(expenses: expenses, date: Calendar.current.date(byAdding: .day, value: -4, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +), isDetailExpenses: $isDetailExpenses)
             }
             //H-5
             if expenses.filter({dayOfDate(for: $0.timestamp ?? Date()) == dayOfDate(for: Calendar.current.date(byAdding: .day, value: -5, to: Date.now) ?? Date())}).count != 0{
-                DailyExpenseView(expenses: expenses, for: Calendar.current.date(byAdding: .day, value: -5, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +))
+                DailyExpenseView(expenses: expenses, date: Calendar.current.date(byAdding: .day, value: -5, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +), isDetailExpenses: $isDetailExpenses)
             }
             //H-6
             if expenses.filter({dayOfDate(for: $0.timestamp ?? Date()) == dayOfDate(for: Calendar.current.date(byAdding: .day, value: -6, to: Date.now) ?? Date())}).count != 0{
-                DailyExpenseView(expenses: expenses, for: Calendar.current.date(byAdding: .day, value: -3, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +))
+                DailyExpenseView(expenses: expenses, date: Calendar.current.date(byAdding: .day, value: -3, to: Date()) ?? Date(), nominal: expenses.map({$0.amount ?? 0}).reduce(0, +), isDetailExpenses: $isDetailExpenses)
             }
         }
         .task {
@@ -52,7 +52,7 @@ struct ExpenseContent: View {
 struct ExpenseContent_Previews: PreviewProvider {
     static var previews: some View {
 
-        ExpenseContent(expenses: .constant([.init(id: UUID(), image: UIImage(), category: "", amount: 0, timestamp: Date.now)]))
+        ExpenseContent(isDetailExpenses: .constant(false), expenses: .constant([.init(id: UUID(), image: UIImage(), category: "", amount: 0, timestamp: Date.now)]))
             .environmentObject(CoreDataViewModel())
     }
 }
